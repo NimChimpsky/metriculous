3476fe74a450d0d67aa1eaff1de0f1f115f06a29
Stephen Batty <sbatty@gmail.com>
2018-06-12 08:56:04 +1000

    V0.5

--- resources/metriculous.js
+++ resources/metriculous.js
@@ -1,8 +1,4 @@
-/**
- * Created by stephenbatty on 11/06/2018.
- */
 function sendContactForm() {
-
     //if you are here this is just to hopefully reduce webscrapinga
     var decoded = atob("aHR0cHM6Ly9mb3Jtc3ByZWUuaW8vc2JhdHR5K21ldHJpY3Vsb3VzQGdtYWlsLmNvbQ==");
     document.getElementById('contact-form').action = decoded;
