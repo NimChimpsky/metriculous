f409359c17365629294caf9b43bda056d5659f88
Stephen Batty <sbatty@gmail.com>
2019-08-15 09:07:27 +1000

    using formspree to persist

--- resources/metriculous.js
+++ resources/metriculous.js
@@ -2,7 +2,6 @@ function sendContactForm() {
     //if you are here this is just to hopefully reduce webscrapinga
     var decoded = atob("aHR0cHM6Ly9mb3Jtc3ByZWUuaW8vc3VwcG9ydCtmb3Jtc3ByZWVAbWV0cmljdWxvdXMubmV0d29yaw==");
     document.getElementById('contact-form').action = decoded;
-
 }
 
 function toggleDownloadForm() {
