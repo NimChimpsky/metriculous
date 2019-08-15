8ee2486698ded05de4b51f0704950e8dc3802bec
Stephen Batty <sbatty@gmail.com>
2019-08-15 12:46:37 +1000

    https://formcarry.com

--- resources/metriculous.js
+++ resources/metriculous.js
@@ -21,11 +21,11 @@ function submitEmailAndDownload() {
     // https://metriculous.network/download.html
     // var decoded_return_url = atob("");
     // var decoded_return_url = btoa("https://metriculous.network/metricloaddown.html");
-    var decoded_return_url = atob("aHR0cHM6Ly9tZXRyaWN1bG91cy5uZXR3b3JrL21ldHJpY2xvYWRkb3duLmh0bWw=");
-    document.getElementById('_after').value = decoded_return_url;
-
-    var decoded_email_download = atob("aHR0cHM6Ly9tYWlsdGhpcy50by9zdXBwb3J0K2Rvd25sb2FkQG1ldHJpY3Vsb3VzLm5ldHdvcms=");
-    document.getElementById('download-form').action = decoded_email_download;
+    // var decoded_return_url = atob("aHR0cHM6Ly9tZXRyaWN1bG91cy5uZXR3b3JrL21ldHJpY2xvYWRkb3duLmh0bWw=");
+    // document.getElementById('_after').value = decoded_return_url;
+    //
+    // var decoded_email_download = atob("aHR0cHM6Ly9tYWlsdGhpcy50by9zdXBwb3J0K2Rvd25sb2FkQG1ldHJpY3Vsb3VzLm5ldHdvcms=");
+    // document.getElementById('download-form').action = decoded_email_download;
 
     //
     // var decoded = atob("aHR0cHM6Ly9mb3Jtc3ByZWUuaW8vc2JhdHR5K21ldHJpY3Vsb3VzQGdtYWlsLmNvbQ==");
